bd3e6543163456330b18167a56b2c383d2b522d3
RilindS <rilindsimnica09@gmail.com>
2024-10-11 21:23:23 +0200

    added configuration

---
+++ src/main/java/com/example/HospitalManagment/data/BaseDTO.java
@@ -0,0 +1,27 @@
+package com.example.HospitalManagment.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class BaseDTO {
+
+    private Long id;
+    private LocalDateTime updatedAt;
+    private LocalDateTime createdAt;
+    @LastModifiedBy
+    private String updatedBy;
+    @CreatedBy
+    private String createdBy;
+
+}
