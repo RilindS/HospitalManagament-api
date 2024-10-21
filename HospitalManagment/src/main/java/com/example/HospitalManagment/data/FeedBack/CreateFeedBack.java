06210d28d444cb94ccbc45a0b5bd2025fa7dd9f6
RilindS <rilindsimnica09@gmail.com>
2024-10-21 22:06:32 +0200

    added feedBack crud

---
+++ HospitalManagment/src/main/java/com/example/HospitalManagment/data/FeedBack/CreateFeedBack.java
@@ -0,0 +1,21 @@
+package com.example.HospitalManagment.data.FeedBack;
+
+import com.example.HospitalManagment.entity.Doctor;
+import com.example.HospitalManagment.entity.Nurse;
+import com.example.HospitalManagment.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateFeedBack {
+    private String comment;
+    private Long rating;
+    private Long doctorId;
+    private Long nurseId;
+    //private User user;
+}
